3d8879018ff167b0b0e3e3b463e13a3c41c03891
unknown <riyad298@gmail.com>
2020-01-04 19:56:18 +0600

    upto props

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Ninjas from './Ninjas';
+
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <p>My first react app</p>
+      <Ninjas home="kurigram" dress='shirt' />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/Ninjas.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+
+function Ninjas(props) {
+    // const { home , dress } = this.props;
+  return (
+    <div className="Ninjas">
+      <p>Name: Riyad</p>
+      <p>A CS graduate</p>
+      <p>Home { props.home }  </p>
+
+    </div>
+  );
+}
+
+export default Ninjas;
